cedca16000b80fcbfc5c4b091268cf66095dc810
garyluu <garylau.work@gmail.com>
2017-09-15 17:23:27 -0400

    Use the new groovy script

--- tooltester/src/main/java/io/dockstore/tooltester/helper/PipelineTester.java
+++ tooltester/src/main/java/io/dockstore/tooltester/helper/PipelineTester.java
@@ -7,7 +7,7 @@ import org.apache.commons.configuration.HierarchicalINIConfiguration;
  * @since 20/01/17
  */
 public class PipelineTester extends JenkinsHelper {
-    private static final String PREFIX = "PipelineTest";
+    private static final String PREFIX = "PipelineTest2";
 
     public PipelineTester(HierarchicalINIConfiguration config) {
         super(config);
