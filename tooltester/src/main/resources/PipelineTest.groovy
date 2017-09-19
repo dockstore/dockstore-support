183b2b0c149d8976462f6851d87cf8bf4121b8ec
garyluu <garylau.work@gmail.com>
2017-09-19 15:51:56 -0400

    Update readme, playbook, groovy script and removed an unncessary playbook

--- tooltester/src/main/resources/PipelineTest.groovy
+++ tooltester/src/main/resources/PipelineTest.groovy
@@ -1,13 +1,16 @@
+currentBuild.displayName = "1.2.8"
 def buildJob = [:]
-buildJob["Build " + params.DockerfilePath] = transformIntoDockerfileStep()
+if (params.Entry == "tool") {
+    buildJob["Build " + params.DockerfilePath] = transformIntoDockerfileStep()
+}
 def ParameterPaths = params.ParameterPath.split(' ')
 def DescriptorPaths = params.DescriptorPath.split(' ')
 for (int i = 0; i < ParameterPaths.length; i++) {
-    buildJob["Test " + ParameterPaths[i]] = transformIntoStep(params.URL, params.Tag, DescriptorPaths[i], ParameterPaths[i])
+    buildJob["Test " + ParameterPaths[i]] = transformIntoStep(params.URL, params.Tag, DescriptorPaths[i], ParameterPaths[i], params.EntryType, params.SynapseCache)
 }
 parallel buildJob
 
-def transformIntoStep(url, tag, descriptor, parameter) {
+def transformIntoStep(url, tag, descriptor, parameter, entryType, synapseCache) {
     // We need to wrap what we return in a Groovy closure, or else it's invoked
     // when this method is called, not when we pass it to parallel.
     // To do this, you need to wrap the code below in { }, and either return
@@ -24,8 +27,11 @@ def transformIntoStep(url, tag, descriptor, parameter) {
                 sh 'git clone ${URL} target'
                 dir('target') {
                     sh 'git checkout ${Tag}'
-                    sh 'echo dockstore tool launch --local-entry ${descriptor} --json ${parameter} --script'
-                    FILE = sh (script: "set -o pipefail && dockstore tool launch --local-entry $descriptor --json $parameter --script | tee /dev/stderr | sed -n -e 's/^.*Saving copy of cwltool stdout to: //p'", returnStdout: true).trim()
+                    if (synapseCache != "") {
+                        sh 's3cmd get --skip-existing --recursive s3://dockstore/test_files/ga4gh-dream/${SynapseCache}/'
+                    }
+                    sh 'echo dockstore ${entryType} launch --local-entry ${descriptor} --json ${parameter} --script'
+                    FILE = sh (script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --json $parameter --script | tee /dev/stderr | sed -n -e 's/^.*Saving copy of cwltool stdout to: //p'", returnStdout: true).trim()
                     sh "mv $FILE $parameter"
                     archiveArtifacts artifacts: parameter
                 }
