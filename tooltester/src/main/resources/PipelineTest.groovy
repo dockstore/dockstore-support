5fa1ac3380bf7aabd26484d25dae80e042e489a5
garyluu <garylau.work@gmail.com>
2018-09-05 16:10:56 -0400

    Workaround for cromwell expecting file relative to current working directory

--- tooltester/src/main/resources/PipelineTest.groovy
+++ tooltester/src/main/resources/PipelineTest.groovy
@@ -1,3 +1,6 @@
+import java.nio.file.Path
+import java.nio.file.Paths
+
 currentBuild.displayName = "1.5.0-beta.5"
 def buildJob = [:]
 if ("tool".equalsIgnoreCase(params.EntryType)) {
@@ -22,7 +25,7 @@ def transformIntoStep(url, tag, descriptor, parameter, entryType, synapseCache) 
                 sh 'rm -rf /mnt/output/*'
                 sh 'rm -rf /media/large_volume/output/*'
                 step([$class: 'WsCleanup'])
-                sh "wget https://raw.githubusercontent.com/ga4gh/dockstore-support/feature/updateCWLTool/tooltester/src/main/resources/${AnsiblePlaybook}.yml"
+                sh "wget https://raw.githubusercontent.com/ga4gh/dockstore-support/feature/playbook/tooltester/src/main/resources/${AnsiblePlaybook}.yml"
                 ansiblePlaybook playbook: '${AnsiblePlaybook}.yml', sudo: true, sudoUser: null
                 sh 'dockstore --version --script || true'
                 sh 'pip list'
@@ -35,13 +38,43 @@ def transformIntoStep(url, tag, descriptor, parameter, entryType, synapseCache) 
                         sh 'aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 cp --recursive s3://dockstore/test_files/${SynapseCache}/ .'
                         // sh 's3cmd get --skip-existing --recursive s3://dockstore/test_files/${SynapseCache}/'
                     }
-                    // Currently determining whether a file is yaml or json based on its file extension
-                    if (parameter.contains('.yml') || parameter.contains('.yaml')) {
-                        sh "echo dockstore ${entryType} launch --local-entry ${descriptor} --yaml ${parameter} --script"
-                        FILE = sh (script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --yaml $parameter --script | sed -n -e 's/^.*Saving copy of .* stdout to: //p'", returnStdout: true).trim()
+
+                    // cromwell resolves the files inside the test parameter file relative to current working directory
+                    // changing working directory to the test parameter file as a workaround
+                    if (JOB_NAME.contains("cromwell")) {
+                        String parameterParent = getParent(parameter)
+                        String descriptorString = getRelativeDescriptorStringPath(parameterParent, descriptor)
+                        String parameterString = Paths.get(parameter).getFileName().toString()
+                        if (parameterParent != "null") {
+                            dir(parameterParent) {
+                                // For some reason, what works for cwltool doesn't work for cromwell
+                                // When cromwell errors out, no logs are produced
+                                if (parameter.contains('.yml') || parameter.contains('.yaml')) {
+                                    sh "echo dockstore ${entryType} launch --local-entry ${descriptorString} --yaml ${parameterString} --script"
+                                    sh "set -o pipefail && dockstore $entryType launch --local-entry $descriptorString --yaml $parameterString --script"
+                                } else {
+                                    sh "echo dockstore ${entryType} launch --local-entry ${descriptorString} --json ${parameterString} --script"
+                                    sh "set -o pipefail && dockstore $entryType launch --local-entry $descriptorString --json $parameterString --script"
+                                }
+                            }
+                        } else {
+                                if (parameter.contains('.yml') || parameter.contains('.yaml')) {
+                                    sh "echo dockstore ${entryType} launch --local-entry ${descriptor} --yaml ${parameter} --script"
+                                    FILE = sh (script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --yaml $parameter --script | sed -n -e 's/^.*Saving copy of .* stdout to: //p'", returnStdout: true).trim()
+                                } else {
+                                    sh "echo dockstore ${entryType} launch --local-entry ${descriptor} --json ${parameter} --script"
+                                    FILE = sh(script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --json $parameter --script | sed -n -e 's/^.*Saving copy of .* stdout to: //p'", returnStdout: true).trim()
+                                }
+                        }
                     } else {
-                        sh "echo dockstore ${entryType} launch --local-entry ${descriptor} --json ${parameter} --script"
-                        FILE = sh (script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --json $parameter --script | sed -n -e 's/^.*Saving copy of .* stdout to: //p'", returnStdout: true).trim()
+                        // Currently determining whether a file is yaml or json based on its file extension (soon to not matter)
+                        if (parameter.contains('.yml') || parameter.contains('.yaml')) {
+                            sh "echo dockstore ${entryType} launch --local-entry ${descriptor} --yaml ${parameter} --script"
+                            FILE = sh (script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --yaml $parameter --script | sed -n -e 's/^.*Saving copy of .* stdout to: //p'", returnStdout: true).trim()
+                        } else {
+                            sh "echo dockstore ${entryType} launch --local-entry ${descriptor} --json ${parameter} --script"
+                            FILE = sh (script: "set -o pipefail && dockstore $entryType launch --local-entry $descriptor --json $parameter --script | sed -n -e 's/^.*Saving copy of .* stdout to: //p'", returnStdout: true).trim()
+                        }
                     }
                     if (JOB_NAME.contains("cwltool") || JOB_NAME.contains("cwl-runner")) {
                         sh "mv $FILE $parameter"
@@ -51,7 +84,6 @@ def transformIntoStep(url, tag, descriptor, parameter, entryType, synapseCache) 
             }
             cleanWs()
         }
-
     }
 }
 
@@ -71,4 +103,16 @@ def transformIntoDockerfileStep(){
             cleanWs()
         }
     }
+}
+
+@NonCPS
+static String getRelativeDescriptorStringPath(String parameterString, String descriptorString) {
+    Path parameterPath = Paths.get(parameterString)
+    Path descriptorPath = Paths.get(descriptorString)
+    return parameterPath.relativize(descriptorPath).toString()
+}
+
+@NonCPS
+static String getParent(String stringPath) {
+    return Paths.get(stringPath).getParent().toString()
 }
