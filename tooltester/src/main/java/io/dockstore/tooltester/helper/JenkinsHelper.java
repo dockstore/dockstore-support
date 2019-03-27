f30532edd8a7e89bd614c0220604436480bf4037
garyluu <garylau.work@gmail.com>
2019-03-27 09:53:09 -0400

    Change for new blue ocean

--- tooltester/src/main/java/io/dockstore/tooltester/helper/JenkinsHelper.java
+++ tooltester/src/main/java/io/dockstore/tooltester/helper/JenkinsHelper.java
@@ -21,6 +21,7 @@ import com.offbytwo.jenkins.model.BuildResult;
 import com.offbytwo.jenkins.model.BuildWithDetails;
 import com.offbytwo.jenkins.model.Job;
 import com.offbytwo.jenkins.model.JobWithDetails;
+import io.dockstore.tooltester.blueOceanJsonObjects.Link;
 import io.dockstore.tooltester.blueOceanJsonObjects.PipelineImpl;
 import io.dockstore.tooltester.blueOceanJsonObjects.PipelineNodeImpl;
 import io.dockstore.tooltester.jenkins.CrumbJsonResult;
@@ -67,7 +68,10 @@ public abstract class JenkinsHelper {
         String entity = getEntity("blue/rest/organizations/jenkins/pipelines/" + name);
         Gson gson = new Gson();
         PipelineImpl example = gson.fromJson(entity, PipelineImpl.class);
-        String uri = example.getLatestRun().getLinks().getNodes().getHref();
+        String uri = example.getLatestRun().getLinks().getSelf().getHref();
+        entity = getEntity(uri);
+        PipelineNodeImpl link = gson.fromJson(entity, PipelineNodeImpl.class);
+        uri = link.getLinks().getNodes().getHref();
         entity = getEntity(uri);
         return gson.fromJson(entity, PipelineNodeImpl[].class);
     }
