93582f7c24ed62bf1e863fe40ada618264bada55
garyluu <garylau.work@gmail.com>
2019-09-18 14:36:30 -0400

    Update blacklist

--- tooltester/src/main/java/io/dockstore/tooltester/blacklist/BlackList.java
+++ tooltester/src/main/java/io/dockstore/tooltester/blacklist/BlackList.java
@@ -1,7 +1,5 @@
 package io.dockstore.tooltester.blacklist;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,7 +12,7 @@ public class BlackList {
      * Determines whether an entry and its version has been blacklisted or not
      * @param entryId   The entry's ID (e.g. #workflow/github.com/dockstore/hello_world)
      * @param version   The entry's version name (e.g. master)
-     * @return
+     * @return Whether or not the tool and toolversion is blacklisted
      */
     public static boolean isNotBlacklisted(String entryId, String version) {
         boolean blacklisted = BlackList.BLACKLIST.stream()
@@ -22,25 +20,32 @@ public class BlackList {
         return !blacklisted;
     }
 
-    private static final List<BlackListObject> BLACKLIST = Collections.unmodifiableList(
-            new ArrayList<BlackListObject>() {{
-                add(new BlackListObject("#workflow/github.com/DataBiosphere/topmed-workflows/UM_variant_caller_wdl", "1.29.0",  "doesn't work"));
-                add(new BlackListObject("#workflow/github.com/DataBiosphere/topmed-workflows/UM_aligner_wdl", "1.29.0", "doesn't work"));
-                add(new BlackListObject("#workflow/github.com/DataBiosphere/topmed-workflows/UM_aligner_cwl", "1.31.0", "doesn't work"));
-                add(new BlackListObject("quay.io/pancancer/pcawg-sanger-cgp-workflow", "2.0.2", "/var/spool/cwl"));
-                add(new BlackListObject("quay.io/pancancer/pcawg-sanger-cgp-workflow", "2.0.3", "/var/spool/cwl"));
-                add(new BlackListObject("quay.io/pancancer/pcawg-bwa-mem-workflow", "2.6.8_1.2", "/var/spool/cwl"));
-                add(new BlackListObject("quay.io/pancancer/pcawg-bwa-mem-workflow", "checker", "/var/spool/cwl"));
-                add(new BlackListObject("quay.io/pancancer/pcawg-dkfz-workflow", "2.0.1_cwl1.0", "/var/spool/cwl"));
-                add(new BlackListObject("quay.io/pancancer/pcawg_delly_workflow", "2.0.0-cwl1.0", "/var/spool/cwl"));
-                add(new BlackListObject("quay.io/pancancer/pcawg_delly_workflow", "2.0.1-cwl1.0", "/var/spool/cwl"));
-                add(new BlackListObject("#workflow/github.com/KnowEnG/cwl-gene-prioritization", "master", "master branch banned"));
-                add(new BlackListObject("#workflow/github.com/NCI-GDC/gdc-dnaseq-cwl/GDC_DNASeq", "master", "doesn't work, master branch banned"));
-                add(new BlackListObject("#workflow/github.com/bcbio/bcbio_validation_workflows", "master", "doesn't work, master branch banned"));
-                add(new BlackListObject("#workflow/github.com/bcbio/bcbio_validation_workflows/wes-agha-test-arvados", "master", "doesn't work, master branch banned"));
-                add(new BlackListObject("#workflow/github.com/dockstore-testing/md5sum-checker", "develop", "input file not relative to parameter file, develop branch banned"));
-                add(new BlackListObject("#workflow/github.com/dockstore-testing/md5sum-checker/wdl", "develop", "input file not relative to parameter file, develop branch banned"));
-                add(new BlackListObject("#workflow/github.com/Barski-lab/ga4gh_challenge", "master", "master branch banned"));
-                add(new BlackListObject("#workflow/github.com/dockstore/hello_world", "master", "master branch banned"));
-            }});
+    private static final List<BlackListObject> BLACKLIST = List
+            .of(
+                    new BlackListObject("#workflow/github.com/DataBiosphere/topmed-workflows/UM_variant_caller_wdl", "1.29.0", "doesn't work"),
+                    new BlackListObject("#workflow/github.com/DataBiosphere/topmed-workflows/UM_aligner_wdl", "1.29.0", "doesn't work"),
+                    new BlackListObject("#workflow/github.com/DataBiosphere/topmed-workflows/UM_aligner_cwl", "1.31.0", "doesn't work"),
+                    new BlackListObject("quay.io/pancancer/pcawg-sanger-cgp-workflow", "2.0.2", "/var/spool/cwl"),
+                    new BlackListObject("quay.io/pancancer/pcawg-sanger-cgp-workflow", "2.0.3", "/var/spool/cwl"),
+                    new BlackListObject("quay.io/pancancer/pcawg-bwa-mem-workflow", "2.6.8_1.2", "/var/spool/cwl"),
+                    new BlackListObject("quay.io/pancancer/pcawg-bwa-mem-workflow", "checker", "/var/spool/cwl"),
+                    new BlackListObject("quay.io/pancancer/pcawg-dkfz-workflow", "2.0.1_cwl1.0", "/var/spool/cwl"),
+                    new BlackListObject("quay.io/pancancer/pcawg_delly_workflow", "2.0.0-cwl1.0", "/var/spool/cwl"),
+                    new BlackListObject("quay.io/pancancer/pcawg_delly_workflow", "2.0.1-cwl1.0", "/var/spool/cwl"),
+                    new BlackListObject("#workflow/github.com/KnowEnG/cwl-gene-prioritization", "master", "master branch banned"),
+                    new BlackListObject("#workflow/github.com/NCI-GDC/gdc-dnaseq-cwl/GDC_DNASeq", "master",
+                            "doesn't work, master branch banned"),
+                    new BlackListObject("#workflow/github.com/bcbio/bcbio_validation_workflows", "master",
+                            "doesn't work, master branch banned"),
+                    new BlackListObject("#workflow/github.com/bcbio/bcbio_validation_workflows/wes-agha-test-arvados", "master",
+                            "doesn't work, master branch banned"),
+                    new BlackListObject("#workflow/github.com/dockstore-testing/md5sum-checker", "develop",
+                            "input file not relative to parameter file, develop branch banned"),
+                    new BlackListObject("#workflow/github.com/dockstore-testing/md5sum-checker/wdl", "develop",
+                            "input file not relative to parameter file, develop branch banned"),
+                    new BlackListObject("#workflow/github.com/Barski-lab/ga4gh_challenge", "master", "master branch banned"),
+                    new BlackListObject("#workflow/github.com/dockstore/hello_world", "master", "master branch banned"),
+                    new BlackListObject("#workflow/github.com/dockstore/hello_world", "master", "master branch banned"),
+                    new BlackListObject("#workflow/github.com/dockstore/hello_world/_cwl_checker:v1.0.0", "v1.0.0",
+                            "not actually a checker workflow"));
 }
