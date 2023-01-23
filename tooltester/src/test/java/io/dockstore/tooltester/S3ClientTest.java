d0e47e53d86ce85edaed33732c4a2c5727c09831
fhembroff <flynnhembroff@gmail.com>
2023-01-23 11:12:49 -0500

    Converted S3ClientTest to Junit 5

--- tooltester/src/test/java/io/dockstore/tooltester/S3ClientTest.java
+++ tooltester/src/test/java/io/dockstore/tooltester/S3ClientTest.java
@@ -2,8 +2,8 @@ package io.dockstore.tooltester;
 
 import java.io.UnsupportedEncodingException;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author gluu
@@ -29,13 +29,13 @@ public class S3ClientTest {
     @Test
     public void convertToolIdToPartialKey() throws UnsupportedEncodingException {
         String toolId = "#workflow/github.com/ENCODE-DCC/pipeline-container/encode-mapping-cwl";
-        Assert.assertEquals("workflow/github.com/ENCODE-DCC/pipeline-container%2Fencode-mapping-cwl", S3Client.convertToolIdToPartialKey(toolId));
+        Assertions.assertEquals("workflow/github.com/ENCODE-DCC/pipeline-container%2Fencode-mapping-cwl", S3Client.convertToolIdToPartialKey(toolId));
         toolId = "#workflow/github.com/ENCODE-DCC/pipeline-container";
-        Assert.assertEquals("workflow/github.com/ENCODE-DCC/pipeline-container", S3Client.convertToolIdToPartialKey(toolId));
+        Assertions.assertEquals("workflow/github.com/ENCODE-DCC/pipeline-container", S3Client.convertToolIdToPartialKey(toolId));
         toolId = "quay.io/pancancer/pcawg-bwa-mem-workflow";
-        Assert.assertEquals("tool/quay.io/pancancer/pcawg-bwa-mem-workflow", S3Client.convertToolIdToPartialKey(toolId));
+        Assertions.assertEquals("tool/quay.io/pancancer/pcawg-bwa-mem-workflow", S3Client.convertToolIdToPartialKey(toolId));
         toolId = "quay.io/pancancer/pcawg-bwa-mem-workflow/thing";
-        Assert.assertEquals("tool/quay.io/pancancer/pcawg-bwa-mem-workflow%2Fthing", S3Client.convertToolIdToPartialKey(toolId));
+        Assertions.assertEquals("tool/quay.io/pancancer/pcawg-bwa-mem-workflow%2Fthing", S3Client.convertToolIdToPartialKey(toolId));
     }
 
 }
