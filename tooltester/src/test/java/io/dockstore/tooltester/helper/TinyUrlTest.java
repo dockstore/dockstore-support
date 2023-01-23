d5f808168ebe2f0a5fb9b9c8b50c72b216ccf6cb
fhembroff <flynnhembroff@gmail.com>
2023-01-23 11:13:43 -0500

    Converted TinyUrlTest to Junit 5

--- tooltester/src/test/java/io/dockstore/tooltester/helper/TinyUrlTest.java
+++ tooltester/src/test/java/io/dockstore/tooltester/helper/TinyUrlTest.java
@@ -1,8 +1,7 @@
 package io.dockstore.tooltester.helper;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author gluu
@@ -13,7 +12,7 @@ public class TinyUrlTest {
     public void getTinyUrl() throws Exception {
         String originalURL = "https://www.google.ca";
         String tinyUrl = TinyUrl.getTinyUrl(originalURL);
-        assertTrue(tinyUrl.equals("https://tinyurl.com/d4gfaxc") || tinyUrl.equals(originalURL));
+        Assertions.assertTrue(tinyUrl.equals("https://tinyurl.com/d4gfaxc") || tinyUrl.equals(originalURL));
     }
 
 }
