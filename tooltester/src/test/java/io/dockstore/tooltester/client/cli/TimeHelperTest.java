0cc8e8541bcb5059214f4ad296ec88f021ed334d
fhembroff <flynnhembroff@gmail.com>
2023-01-24 11:16:35 -0500

    Removed Assertions. from all tests in TimeHelperTest

--- tooltester/src/test/java/io/dockstore/tooltester/client/cli/TimeHelperTest.java
+++ tooltester/src/test/java/io/dockstore/tooltester/client/cli/TimeHelperTest.java
@@ -4,8 +4,10 @@ import java.text.ParseException;
 import java.util.Objects;
 
 import io.dockstore.tooltester.helper.TimeHelper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author gluu
@@ -15,7 +17,7 @@ public class TimeHelperTest {
     @Test
     public void durationToString() throws Exception {
         String durationString = TimeHelper.durationToString(Long.valueOf(3700000));
-        Assertions.assertTrue(durationString.equals("1h 2m"), "Incorrect time calculated, expected \"1h 2m\" but got " + durationString);
+        assertTrue(durationString.equals("1h 2m"), "Incorrect time calculated, expected \"1h 2m\" but got " + durationString);
     }
 
     @Test
@@ -26,14 +28,14 @@ public class TimeHelperTest {
         } catch (ParseException e) {
             System.out.println("Could not convert date");
         }
-        Assertions.assertTrue(Objects.equals(time, "2017-02-22 15:36"), "Incorrect date calculated, expected \"2017-02-22 15:36\" but got " + time);
+        assertTrue(Objects.equals(time, "2017-02-22 15:36"), "Incorrect date calculated, expected \"2017-02-22 15:36\" but got " + time);
     }
 
     @Test
     public void datetimeToEpoch() {
         String startTime = "2019-04-05T15:21:44.219+0000";
         String epochTimeString = TimeHelper.timeFormatToEpoch(startTime);
-        Assertions.assertEquals("1554477704219", epochTimeString);
+        assertEquals("1554477704219", epochTimeString);
     }
 
 
