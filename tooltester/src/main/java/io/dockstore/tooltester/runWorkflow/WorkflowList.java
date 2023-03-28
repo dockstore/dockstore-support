38b211006f3fd12fa3aba14b7193739356cfe1f3
fhembroff <flynnhembroff@gmail.com>
2023-03-27 20:27:49 -0400

    Comment all WDL workflows

--- tooltester/src/main/java/io/dockstore/tooltester/runWorkflow/WorkflowList.java
+++ tooltester/src/main/java/io/dockstore/tooltester/runWorkflow/WorkflowList.java
@@ -45,11 +45,11 @@ public class WorkflowList {
     private final List<WorkflowRunner> workflowsToTest;
 
     private List<WorkflowRunner> getDefaultTests() throws InterruptedException {
-        return List.of(new WorkflowRunner("github.com/dockstore-testing/wes-testing/agc-fastq-read-counts", "main", "test-parameter-files/agc-fastq-read-counts-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
+        return List.of(/*new WorkflowRunner("github.com/dockstore-testing/wes-testing/agc-fastq-read-counts", "main", "test-parameter-files/agc-fastq-read-counts-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
                         new WorkflowRunner("github.com/dockstore-testing/wes-testing/agc-fastq-read-counts", "main", "agc-examples/fastq/input.json", ga4Ghv20Api, extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
                         new WorkflowRunner("github.com/gatk-workflows/seq-format-conversion/BAM-to-Unmapped-BAM", "3.0.0", "test-parameter-files/BAM-to-Unmapped-BAM-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
                         new WorkflowRunner("github.com/manning-lab/vcfToGds", "main", "test-parameter-files/vcfToGds-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
-                        new WorkflowRunner("github.com/DataBiosphere/analysis_pipeline_WDL/assocation-aggregate-wdl", "v7.1.1", "test-parameter-files/assocation-aggregate-wdl-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
+                        new WorkflowRunner("github.com/DataBiosphere/analysis_pipeline_WDL/assocation-aggregate-wdl", "v7.1.1", "test-parameter-files/assocation-aggregate-wdl-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),*/
                         new WorkflowRunner("github.com/fhembroff/wes-testing/nontrivial", "stable-version-for-testing-v1", "test-parameter-files/nontrivial-test-parameter-file.json", extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
                         new WorkflowRunner("github.com/fhembroff/wes-testing/manyJobs", "stable-version-for-testing-v1", "manyjobs/inputs.tiny.json", ga4Ghv20Api, extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL),
                         new WorkflowRunner("github.com/fhembroff/wes-testing/manyJobs", "stable-version-for-testing-v1", "manyjobs/inputs.json", ga4Ghv20Api, extendedGa4GhApi, workflowsApi, configFilePathWDL, configFilePathCWL)
