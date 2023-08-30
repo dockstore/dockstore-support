63a74b39c36cdf86eea272bb09c078864c731b62
Charles Overbeck <charles.overbeck@gmail.com>
2023-08-30 09:12:34 -0700

    Some JIRA Automation (#473)

---
+++ jira_automation/README.md
@@ -0,0 +1,25 @@
+There are three applications in here to facilitate our JIRA/GitHub interaction
+
+1. io.dockstore.jira.MilestoneChecker - generates GitHub and JQL queries to find mismatches in the JIRA fix version and GitHub milestone. The JIRA fix version is
+   a multi-value field; the GitHub milestone is a single-value field, so Unito doesn't sync them. We have to remember to manually
+   keep them in sync; this program identifies cases we've missed.
+2. SprintStart - a barely started work in progress to automatically generate review tickets at the beginning of a sprint, which
+   is currently a manual and tedious process.
+3. io.dockstore.jira.ResolutionChecker - used to help find issues open in GitHub that are closed in JIRA. This was to diagnose an issue where 
+Unito was seemingly mysteriously closing JIRA issues at random. It turned out to be because we hadn't properly configured
+a GitHub and JIRA user in Unito -- it's the Unito intended behavior. We currently don't need to run this, although if we have
+a configuration issue again, it could be useful in the future.
+
+# Auth
+
+* ResolutionChecker and MilestoneCheck require environment variable `GITHUB_TOKEN` be set to a GitHub personal access token.
+* SprintStart requires the environment variable `JIRA_TOKEN` be set to a JIRA token.
+
+# Usage
+
+I usually run in IntelliJ with a Run Configuration
+
+1. In Run Configuration, set the main class to io.dockstore.jira.MilestoneChecker or io.dockstore.jira.ResolutionChecker
+2. Add the environment variable `GITHUB_TOKEN` to your GitHub token.
+3. The console will print out generated queries, which you then paste into your browser.
+
---
+++ jira_automation/src/main/java/io/dockstore/jira/MilestoneChecker.java
@@ -0,0 +1,72 @@
+package io.dockstore.jira;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHMilestone;
+
+/**
+ * Generates a GitHub url with all dockstore issues whose milestone does not match the fix version
+ * in JIRA. Works by looking at all open GitHub issues, then reading the info Unito appends to the
+ * description in GitHub, which includes the fix version. It compares the fix version in JIRA with
+ * the milestone of the GitHub issue.
+ */
+public final class MilestoneChecker {
+
+    /**
+     *  Pattern for finding a substring like "Fix Versions: Dockstore 1.15" in GitHub issue body, to extract
+     *  the "1.15"
+      */
+    private static final Pattern FIX_VERSIONS = Pattern.compile("((Fix Versions)|(fixVersions))(:\\s*(Dockstore )?(.*))");
+
+    private MilestoneChecker() { }
+
+    public static void main(String[] args) throws IOException {
+        final List<GHIssue> openIssues = Utils.findOpenIssues(Utils.getDockstoreRepository());
+        final List<JiraAndGithub> issues = openIssues.stream()
+            .filter(ghIssue -> {
+                final GHMilestone milestone = ghIssue.getMilestone();
+                final String body = ghIssue.getBody();
+                final Matcher matcher = FIX_VERSIONS.matcher(body);
+                if (matcher.find()) {
+                    if (milestone == null) {
+                        // There's a fix version in JIRA, but none in GitHub
+                        return true;
+                    }
+                    final String jiraFixVersion = matcher.group(6);
+                    return !milestoneAndFixVersionEqual(jiraFixVersion, milestone.getTitle());
+                } else {
+                    // No fix version in JIRA, is there one in Dockstore?
+                    return milestone != null;
+                }
+            })
+            .map(ghIssue -> new JiraAndGithub(Utils.findJiraIssueInBody(ghIssue).get(), ghIssue.getNumber()))
+            .collect(Collectors.toList());
+        System.out.println(generateGitHubIssuesUrl(issues));
+        System.out.println();
+        System.out.println(generateJiraIssuesUrl(issues));
+    }
+
+    private static String generateJiraIssuesUrl(final List<JiraAndGithub> issues) {
+        return "https://ucsc-cgl.atlassian.net/issues/?jql=project=DOCK AND "
+            + issues.stream().map(issue -> "key=\"" + issue.jiraIssue() + "\"")
+                .collect(Collectors.joining(" or "));
+    }
+
+    private static String generateGitHubIssuesUrl(final List<JiraAndGithub> issues) {
+        return "https://github.com/dockstore/dockstore/issues?q="
+            + issues.stream().map(issue -> "" + issue.githubIssue())
+            .collect(Collectors.joining("+"));
+    }
+
+    private static boolean milestoneAndFixVersionEqual(String jiraFixVersion, String milestone) {
+        return "Open-ended research tasks".equals(jiraFixVersion) && "Open ended research tasks".equals(milestone)
+            || Objects.equals(jiraFixVersion, milestone);
+    }
+
+    record JiraAndGithub(String jiraIssue, int githubIssue) { }
+}
