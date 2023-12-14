c0277ac200d074290b22788bd3c8b7669bef3f30
Kathy Tran <kathylevtran@gmail.com>
2023-12-14 13:11:38 -0500

    Fix bug where executions were skipped when they shouldn't be skipped

--- metricsaggregator/src/main/java/io/dockstore/metricsaggregator/client/cli/TerraMetricsSubmitter.java
+++ metricsaggregator/src/main/java/io/dockstore/metricsaggregator/client/cli/TerraMetricsSubmitter.java
@@ -21,6 +21,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -31,7 +32,6 @@ import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -47,13 +47,22 @@ import org.slf4j.LoggerFactory;
 
 public class TerraMetricsSubmitter {
     private static final Logger LOG = LoggerFactory.getLogger(MetricsAggregatorClient.class);
+    private static final int MAX_NUMBER_OF_MICRO_SECONDS = 9;
+    // Example workflow_start: 2022-07-15 15:37:06.450000 UTC
+    private static final DateTimeFormatter WORKFLOW_START_FORMAT = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd HH:mm:ss")
+            .appendFraction(ChronoField.MICRO_OF_SECOND, 0, MAX_NUMBER_OF_MICRO_SECONDS, true) // There are varying widths of micro seconds
+            .optionalStart() // Start optional time zone pattern. Time zone is not always included
+            .appendPattern(" ")
+            .appendZoneId()
+            .optionalEnd() // End optional time zone pattern
+            .toFormatter();
     private final MetricsAggregatorConfig config;
     private final SubmitTerraMetrics submitTerraMetricsCommand;
     private final AtomicInteger numberOfExecutionsProcessed = new AtomicInteger(0);
     private final AtomicInteger numberOfExecutionsSubmitted = new AtomicInteger(0);
     private final AtomicInteger numberOfExecutionsSkipped = new AtomicInteger(0);
-    private final AtomicInteger numberOfCacheHits = new AtomicInteger(0);
-    private final AtomicInteger numberOfCacheMisses = new AtomicInteger(0);
+
     // Keep track of sourceUrls that are found to be ambiguous
     private final Map<String, String> sourceUrlsToSkipToReason = new ConcurrentHashMap<>();
     // Map of source url to TRS info that was previously calculated
@@ -74,7 +83,6 @@ public class TerraMetricsSubmitter {
         Iterable<CSVRecord> workflowMetricRecords = null;
         final String inputDateFilePath = this.submitTerraMetricsCommand.getDataFilePath();
         try (BufferedReader metricsBufferedReader = new BufferedReader(new FileReader(inputDateFilePath))) {
-            //final Reader entriesCsv = new BufferedReader(new FileReader(inputDateFilePath));
             final CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
                     .setHeader(TerraMetricsCsvHeaders.class)
                     .setSkipHeaderRecord(true)
@@ -92,11 +100,13 @@ public class TerraMetricsSubmitter {
                         workflowMetricsToProcess.add(workflowMetricRecord);
                     } else {
                         workflowMetricsToProcess.add(workflowMetricRecord);
-                        LOG.info("Processing {} rows", workflowMetricsToProcess.size());
+                        LOG.info("Processing rows {} to {}", workflowMetricsToProcess.get(0).getRecordNumber(), workflowMetricsToProcess.get(workflowMetricsToProcess.size() - 1).getRecordNumber());
+
                         workflowMetricsToProcess.stream()
                                 .parallel()
                                 .forEach(record -> processWorkflowExecution(record, workflowsApi, extendedGa4GhApi, skippedExecutionsCsvPrinter));
                         workflowMetricsToProcess.clear();
+                        logStats();
                     }
                 }
             } catch (IOException e) {
@@ -104,7 +114,8 @@ public class TerraMetricsSubmitter {
                 System.exit(FAILURE_EXIT_CODE);
             }
 
-            LOG.info("Done processing {} executions from Terra. Submitted {} executions. Skipped {} executions. Cache hits: {}. Cache misses: {}", numberOfExecutionsProcessed, numberOfExecutionsSubmitted, numberOfExecutionsSkipped, numberOfCacheHits, numberOfCacheMisses);
+            logStats();
+
             if (submitTerraMetricsCommand.isRecordSkippedExecutions()) {
                 LOG.info("View skipped executions in file {}", outputFileName);
             }
@@ -114,6 +125,10 @@ public class TerraMetricsSubmitter {
         }
     }
 
+    private void logStats() {
+        LOG.info("New way: Done processing {} executions from Terra. Submitted {} executions. Skipped {} executions.", numberOfExecutionsProcessed, numberOfExecutionsSubmitted, numberOfExecutionsSkipped);
+    }
+
     private void processWorkflowExecution(CSVRecord workflowMetricRecord, WorkflowsApi workflowsApi, ExtendedGa4GhApi extendedGa4GhApi, CSVPrinter skippedExecutionsCsvPrinter) {
         final String sourceUrl = workflowMetricRecord.get(TerraMetricsCsvHeaders.source_url);
         LOG.info("Processing execution on row {} with source_url {}", workflowMetricRecord.getRecordNumber(), sourceUrl);
@@ -121,26 +136,23 @@ public class TerraMetricsSubmitter {
 
         // Check to see if this source_url was skipped before
         if (sourceUrlsToSkipToReason.containsKey(sourceUrl)) {
-            skipExecution(sourceUrl, workflowMetricRecord, sourceUrlsToSkipToReason.get(sourceUrl), skippedExecutionsCsvPrinter);
+            skipExecution(sourceUrl, workflowMetricRecord, sourceUrlsToSkipToReason.get(sourceUrl), skippedExecutionsCsvPrinter, true);
             return;
         }
 
         // Check to see if we need to figure out the TRS ID for the source URL
-        if (sourceUrlToSourceUrlTrsInfo.containsKey(sourceUrl)) {
-            numberOfCacheHits.incrementAndGet();
-        } else {
-            numberOfCacheMisses.incrementAndGet();
+        Optional<RunExecution> workflowExecution = getTerraWorkflowExecutionFromCsvRecord(workflowMetricRecord, sourceUrl, skippedExecutionsCsvPrinter);
+        if (workflowExecution.isEmpty()) {
+            return; // Row is invalid, don't figure out TRS ID
+        }
+
+        if (!sourceUrlToSourceUrlTrsInfo.containsKey(sourceUrl)) {
             Optional<SourceUrlTrsInfo> sourceUrlTrsInfo = calculateTrsInfoFromSourceUrl(workflowMetricRecord, sourceUrl, workflowsApi, skippedExecutionsCsvPrinter);
             if (sourceUrlTrsInfo.isEmpty()) {
                 return;
             } else {
                 sourceUrlToSourceUrlTrsInfo.put(sourceUrl, sourceUrlTrsInfo.get());
             }
-        }
-
-        final Optional<RunExecution> workflowExecution = getTerraWorkflowExecutionFromCsvRecord(workflowMetricRecord, sourceUrl, skippedExecutionsCsvPrinter);
-        if (workflowExecution.isEmpty()) {
-            return;
         }
 
         final SourceUrlTrsInfo sourceUrlTrsInfo = sourceUrlToSourceUrlTrsInfo.get(sourceUrl);
@@ -150,7 +162,7 @@ public class TerraMetricsSubmitter {
                     "Terra metrics from Q4 2023");
             numberOfExecutionsSubmitted.incrementAndGet();
         } catch (ApiException e) {
-            skipExecution(sourceUrl, workflowMetricRecord, String.format("Could not submit execution metrics to Dockstore for workflow %s: %s", sourceUrlTrsInfo, e.getMessage()), skippedExecutionsCsvPrinter);
+            skipExecution(sourceUrl, workflowMetricRecord, String.format("Could not submit execution metrics to Dockstore for workflow %s: %s", sourceUrlTrsInfo, e.getMessage()), skippedExecutionsCsvPrinter, false);
         }
     }
 
@@ -164,32 +176,39 @@ public class TerraMetricsSubmitter {
         return Optional.ofNullable(executionStatusEnum);
     }
 
-    static Optional<String> formatStringInIso8601Date(String workflowStart) {
-        // Example workflow_start: 2022-07-15 15:37:06.450000 UTC
-        final int maxNumberOfMicroSeconds = 9;
-        final DateTimeFormatter workflowStartFormat = new DateTimeFormatterBuilder()
-                .appendPattern("yyyy-MM-dd HH:mm:ss")
-                .appendFraction(ChronoField.MICRO_OF_SECOND, 0, maxNumberOfMicroSeconds, true) // There are varying widths of micro seconds
-                .optionalStart() // Start optional time zone pattern. Time zone is not always included
-                .appendPattern(" ")
-                .appendZoneId()
-                .optionalEnd() // End optional time zone pattern
-                .toFormatter();
-
+    private Optional<String> getDateExecuted(CSVRecord csvRecord, String workflowStart, String sourceUrl, CSVPrinter skippedExecutionsCsvPrinter) {
         try {
-            final LocalDateTime localDateTime = LocalDateTime.parse(workflowStart, workflowStartFormat);
+            return Optional.of(getIso8601Date(workflowStart));
+        } catch (DateTimeException e) {
+            skipExecution(sourceUrl, csvRecord, String.format("Could not get valid dateExecuted from workflow start '%s': %s", workflowStart, e), skippedExecutionsCsvPrinter, false);
+            return Optional.empty();
+        }
+    }
+
+    static String getIso8601Date(String workflowStart) throws DateTimeException {
+        final LocalDateTime localDateTime = LocalDateTime.parse(workflowStart, WORKFLOW_START_FORMAT);
+        return DateTimeFormatter.ISO_INSTANT.format(localDateTime.atOffset(ZoneOffset.UTC));
+    }
+
+
+    static Optional<String> formatStringInIso8601Date(String workflowStart) {
+        try {
+            final LocalDateTime localDateTime = LocalDateTime.parse(workflowStart, WORKFLOW_START_FORMAT);
             return Optional.of(DateTimeFormatter.ISO_INSTANT.format(localDateTime.atOffset(ZoneOffset.UTC)));
         } catch (DateTimeParseException e) {
+            LOG.error("Could not format workflow_start '{}' in ISO 8601 date format", workflowStart, e);
             return Optional.empty();
         }
     }
 
     private Optional<String> getPrimaryDescriptorAbsolutePath(WorkflowsApi workflowsApi, MinimalWorkflowInfo workflow, String version) {
+        Optional<String> primaryDescriptorPath = Optional.empty();
         try {
-            return Optional.of(workflowsApi.primaryDescriptor1(workflow.id(), version, workflow.descriptorType().toString()).getAbsolutePath());
+            primaryDescriptorPath = Optional.of(workflowsApi.primaryDescriptor1(workflow.id(), version, workflow.descriptorType().toString()).getAbsolutePath());
         } catch (ApiException e) {
-            return Optional.empty();
+            LOG.debug("Could not get primary descriptor path", e);
         }
+        return primaryDescriptorPath;
     }
 
 
@@ -201,10 +220,14 @@ public class TerraMetricsSubmitter {
                 .toList();
     }
 
-    private void skipExecution(String sourceUrl, CSVRecord csvRecordToSkip, String reason, CSVPrinter skippedExecutionsCsvPrinter) {
-        LOG.info("Skipping execution on row {}: {}", csvRecordToSkip.getRecordNumber(), reason);
-        // Record to map for future reference
-        sourceUrlsToSkipToReason.put(sourceUrl, reason);
+    private void skipExecution(String sourceUrl, CSVRecord csvRecordToSkip, String reason, CSVPrinter skippedExecutionsCsvPrinter, boolean skipFutureExecutionsWithSourceUrl) {
+        LOG.info("Skipping execution on row {} with record {}: {}", csvRecordToSkip.getRecordNumber(), csvRecordToSkip.values(), reason);
+
+        // Record to map for future reference. Only want to do this if the skip reason applies for ALL executions with the source_url.
+        // Should not add to this map if the skip reason is specific to one execution
+        if (skipFutureExecutionsWithSourceUrl) {
+            sourceUrlsToSkipToReason.put(sourceUrl, reason);
+        }
         if (submitTerraMetricsCommand.isRecordSkippedExecutions()) {
             // Record to output CSV file for later examination
             // Headers: workflow_id, status, workflow_start, workflow_end, workflow_runtime_minutes, source_url
@@ -228,21 +251,31 @@ public class TerraMetricsSubmitter {
         final String workflowRunTimeMinutes = csvRecord.get(TerraMetricsCsvHeaders.workflow_runtime_minutes);
 
         // Check that all required fields are non-null
-        if (executionId == null || workflowStart == null || status == null) {
-            skipExecution(sourceUrl, csvRecord, "One or more of the required fields (workflow_id, workflow_start, status) is missing", skippedExecutionsCsvPrinter);
+        if (StringUtils.isBlank(executionId)) {
+            skipExecution(sourceUrl, csvRecord, "The required field workflow_id is missing", skippedExecutionsCsvPrinter, false);
+            return Optional.empty();
+        }
+
+        if (StringUtils.isBlank(workflowStart)) {
+            skipExecution(sourceUrl, csvRecord, "The required field workflow_start is missing", skippedExecutionsCsvPrinter, false);
+            return Optional.empty();
+        }
+
+        if (StringUtils.isBlank(status)) {
+            skipExecution(sourceUrl, csvRecord, "The required field status is missing", skippedExecutionsCsvPrinter, false);
             return Optional.empty();
         }
 
         // Format fields into Dockstore schema
         final Optional<ExecutionStatusEnum> executionStatus = getExecutionStatusEnumFromTerraStatus(status);
         if (executionStatus.isEmpty()) {
-            skipExecution(sourceUrl, csvRecord, "Could not get a valid ExecutionStatusEnum from status '" + status + "'", skippedExecutionsCsvPrinter);
+            skipExecution(sourceUrl, csvRecord, "Could not get a valid ExecutionStatusEnum from status '" + status + "'", skippedExecutionsCsvPrinter, false);
             return Optional.empty();
         }
 
-        final Optional<String> dateExecuted = formatStringInIso8601Date(workflowStart);
+        final Optional<String> dateExecuted = getDateExecuted(csvRecord, workflowStart, sourceUrl, skippedExecutionsCsvPrinter);
         if (dateExecuted.isEmpty()) {
-            skipExecution(sourceUrl, csvRecord, "Could not get a valid dateExecuted from workflow_start '" + workflowStart + "'", skippedExecutionsCsvPrinter);
+            skipExecution(sourceUrl, csvRecord, "Could not get a valid dateExecuted from workflow_start '" + workflowStart + "'", skippedExecutionsCsvPrinter, false);
             return Optional.empty();
         }
 
@@ -281,13 +314,13 @@ public class TerraMetricsSubmitter {
             if (!workflowPathPrefixToWorkflows.containsKey(workflowPathPrefix)) {
                 try {
                     List<MinimalWorkflowInfo> publishedWorkflowsWithSamePathPrefix = workflowsApi.getAllPublishedWorkflowByPath(workflowPathPrefix).stream()
-                            .map(workflow -> new MinimalWorkflowInfo(workflow.getId(), workflow.getFullWorkflowPath(), workflow.getDescriptorType(), new HashMap<>()))
+                            .map(workflow -> new MinimalWorkflowInfo(workflow.getId(), workflow.getFullWorkflowPath(), workflow.getDescriptorType(), new ConcurrentHashMap<>()))
                             .toList();
                     workflowPathPrefixToWorkflows.put(workflowPathPrefix, publishedWorkflowsWithSamePathPrefix);
                 } catch (ApiException e) {
                     skipExecution(sourceUrl, workflowMetricRecord,
                             "Could not get all published workflows for workflow path " + workflowPathPrefix + " to determine TRS ID",
-                            skippedExecutionsCsvPrinter);
+                            skippedExecutionsCsvPrinter, true);
                     return Optional.empty();
                 }
             }
@@ -298,8 +331,8 @@ public class TerraMetricsSubmitter {
             // Loop through each workflow to find one that matches the primary descriptor
             workflowsFromSameRepo.forEach(workflow -> {
                 if (!workflow.versionToPrimaryDescriptorPath().containsKey(version)) {
-                    // Intentionally putting null in map to indicate that the primary descriptor path for the workflow version doesn't exist
-                    workflow.versionToPrimaryDescriptorPath().put(version, getPrimaryDescriptorAbsolutePath(workflowsApi, workflow, version).orElse(null));
+                    // Intentionally putting empty string in map to indicate that the primary descriptor path for the workflow version doesn't exist
+                    workflow.versionToPrimaryDescriptorPath().put(version, getPrimaryDescriptorAbsolutePath(workflowsApi, workflow, version).map(this::makePathAbsolute).orElse(""));
                 }
 
                 final String primaryDescriptorPathForVersion = workflow.versionToPrimaryDescriptorPath().get(version);
@@ -311,12 +344,12 @@ public class TerraMetricsSubmitter {
 
 
             if (foundFullWorkflowPaths.isEmpty()) {
-                skipExecution(sourceUrl, workflowMetricRecord, "Could not find workflow with primary descriptor " + primaryDescriptorPathFromUrl, skippedExecutionsCsvPrinter);
+                skipExecution(sourceUrl, workflowMetricRecord, "Could not find workflow with primary descriptor " + primaryDescriptorPathFromUrl, skippedExecutionsCsvPrinter, true);
                 return Optional.empty();
             } else if (foundFullWorkflowPaths.size() > 1) {
                 // There is already a workflow in the same repository with the same descriptor path that we're looking for.
                 // Skip this source url because it is an ambiguous case and we can't identify which workflow the source url is referring to.
-                skipExecution(sourceUrl, workflowMetricRecord, String.format("There's %s workflows in the repository with the same primary descriptor path '%s': %s", foundFullWorkflowPaths.size(), primaryDescriptorPathFromUrl, foundFullWorkflowPaths), skippedExecutionsCsvPrinter);
+                skipExecution(sourceUrl, workflowMetricRecord, String.format("There's %s workflows in the repository with the same primary descriptor path '%s': %s", foundFullWorkflowPaths.size(), primaryDescriptorPathFromUrl, foundFullWorkflowPaths), skippedExecutionsCsvPrinter, true);
                 return Optional.empty();
             } else {
                 final SourceUrlTrsInfo sourceUrlTrsInfo = new SourceUrlTrsInfo(sourceUrl, "#workflow/" + foundFullWorkflowPaths.get(0), version);
@@ -324,9 +357,13 @@ public class TerraMetricsSubmitter {
             }
 
         } else {
-            skipExecution(sourceUrl, workflowMetricRecord, "Not enough components in the source_url to figure out the TRS ID and version", skippedExecutionsCsvPrinter);
+            skipExecution(sourceUrl, workflowMetricRecord, "Not enough components in the source_url to figure out the TRS ID and version", skippedExecutionsCsvPrinter, true);
             return Optional.empty();
         }
+    }
+
+    private String makePathAbsolute(String path) {
+        return path.startsWith("/") ? path : "/" + path;
     }
 
     /**
@@ -338,6 +375,6 @@ public class TerraMetricsSubmitter {
     public record SourceUrlTrsInfo(String sourceUrl, String trsId, String version) {
     }
 
-    public record MinimalWorkflowInfo(long id, String fullWorkflowPath, DescriptorTypeEnum descriptorType, Map<String, String> versionToPrimaryDescriptorPath) {
+    public record MinimalWorkflowInfo(long id, String fullWorkflowPath, DescriptorTypeEnum descriptorType, ConcurrentHashMap<String, String> versionToPrimaryDescriptorPath) {
     }
 }
