c21b8c1b14aada3dcc7df83dea8aec243cecf711
hyunnaye <nayeonhyun802@gmail.com>
2024-05-09 10:19:09 -0400

    test

---
+++ githubdelivery/src/main/java/io/dockstore/githubdelivery/GithubDeliveryS3Client.java
@@ -0,0 +1,34 @@
+package io.dockstore.githubdelivery;
+
+import io.dockstore.common.S3ClientHelper;
+import scala.util.parsing.json.JSONObject;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GithubDeliveryS3Client {
+    private static final Logger LOG = LoggerFactory.getLogger(GithubDeliveryS3Client.class);
+    private final S3Client s3Client;
+    private final String bucketName;
+
+    public GithubDeliveryS3Client(String bucketName) {
+        this.bucketName = bucketName;
+        this.s3Client = S3ClientHelper.getS3Client();
+    }
+
+    private GetObjectResponse getGitHubDeliveryEventByKey(String key) {
+        GetObjectRequest objectRequest = GetObjectRequest
+                .builder()
+                .key(key)
+                .bucket(bucketName)
+                .build();
+
+        return this.s3Client.getObject(objectRequest);
+
+    }
+}
