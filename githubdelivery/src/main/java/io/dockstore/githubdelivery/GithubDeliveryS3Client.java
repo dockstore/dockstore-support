f6890dbf370a3a8d83633a09baa2e2e10ff552f9
hyunnaye <nayeonhyun802@gmail.com>
2024-06-07 15:54:49 -0400

    changes

--- githubdelivery/src/main/java/io/dockstore/githubdelivery/GithubDeliveryS3Client.java
+++ githubdelivery/src/main/java/io/dockstore/githubdelivery/GithubDeliveryS3Client.java
@@ -118,8 +118,7 @@ public class GithubDeliveryS3Client {
 
         try {
             PushPayload pushPayload;
-            System.out.println(IOUtils.toString(object, StandardCharsets.UTF_8));
-            pushPayload = MAPPER.readValue(object, PushPayload.class);
+            pushPayload = MAPPER.readValue(IOUtils.toString(object, StandardCharsets.UTF_8), PushPayload.class);
             return pushPayload;
         } catch (JsonSyntaxException e) {
             LOG.error("Could not read github event from key {}", key, e);
