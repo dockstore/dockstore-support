be42a5377b103736b715b6df0d103f1f67d0dbf1
Charles Overbeck <coverbec@ucsc.edu>
2024-08-16 09:36:40 -0700

    Pr feedback

--- githubdelivery/src/main/java/io/dockstore/githubdelivery/GithubDeliveryHelper.java
+++ githubdelivery/src/main/java/io/dockstore/githubdelivery/GithubDeliveryHelper.java
@@ -1,12 +1,13 @@
 package io.dockstore.githubdelivery;
 
+import static io.dockstore.utils.ExceptionHandler.exceptionMessage;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonSyntaxException;
 import io.dockstore.openapi.client.model.InstallationRepositoriesPayload;
 import io.dockstore.openapi.client.model.PushPayload;
 import io.dockstore.openapi.client.model.ReleasePayload;
-import static io.dockstore.utils.ExceptionHandler.exceptionMessage;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,20 +26,32 @@ final class GithubDeliveryHelper {
     private GithubDeliveryHelper() {
     }
 
-    static PushPayload getGitHubPushPayloadByKey(String eventType, String body, String key) throws IOException, NoSuchKeyException {
+    static PushPayload getGitHubPushPayloadByKey(String eventType, String body, String key) throws IOException {
         return getPayloadByKey(eventType, body, key, PushPayload.class);
     }
 
-    static InstallationRepositoriesPayload getGitHubInstallationRepositoriesPayloadByKey(String eventType, String body, String key) throws IOException, NoSuchKeyException {
+    static InstallationRepositoriesPayload getGitHubInstallationRepositoriesPayloadByKey(String eventType, String body, String key) throws IOException {
         return getPayloadByKey(eventType, body, key, InstallationRepositoriesPayload.class);
     }
 
-    static ReleasePayload getGitHubReleasePayloadByKey(String eventType, String body, String key) throws IOException, NoSuchKeyException {
+    static ReleasePayload getGitHubReleasePayloadByKey(String eventType, String body, String key) throws IOException {
         return getPayloadByKey(eventType, body, key, ReleasePayload.class);
     }
 
-    static <T> T getPayloadByKey(String eventType, String body, String key, Class<T> clazz) throws IOException,
-        NoSuchKeyException {
+    /**
+     * Deserializes JSON into an object of type <code>T</code>. Returns <code>null</code> if it
+     * fails.
+     *
+     * @param eventType the event type, e.g., push, release, only used for logging an error
+     * @param body the JSON to deserialize
+     * @param key the AWS S3 key where the body came from, only used for logging an error
+     * @param clazz - the Java class to deserialize to
+     * @return
+     * @param <T>
+     * @throws IOException
+     * @throws NoSuchKeyException
+     */
+    static <T> T getPayloadByKey(String eventType, String body, String key, Class<T> clazz) throws IOException {
         try {
             T payload = MAPPER.readValue(body, clazz);
             if (payload == null) {
