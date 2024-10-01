a69e43007b5cf39393bf0cf233ba63fe66f6f10e
Kathy Tran <kathylevtran@gmail.com>
2024-10-01 13:45:30 -0400

    Clean up

--- topicgenerator/scripts/updateDockstoreTopics.sh
+++ topicgenerator/scripts/updateDockstoreTopics.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
-set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# This script gets entries from Dockstore that are AI topic candidates, generates topics for them, then uploads the topics to Dockstore.
 
 TOPIC_GENERATOR_VERSION=${1}
+TOPIC_GENERATOR_CONFIG_PATH=${2}
 
-if [ -z "${TOPIC_GENERATOR_VERSION}" ]; then
-  echo "Usage: ${0} <topic-generator-version>"
-  echo "Example: ${0} 1.16.0-alpha.4"
+if [ -z "${TOPIC_GENERATOR_VERSION}"] || [ -z "${TOPIC_GENERATOR_CONFIG_PATH}" ]; then
+  echo "Usage: ${0} <topic-generator-version> <topic-generator-config-path>"
+  echo "Example: ${0} 1.16.0-alpha.4 ./topic-generator.config"
   exit 1
 fi
 
-wget https://artifacts.oicr.on.ca:443/artifactory/collab-release/io/dockstore/topicgenerator/"${TOPIC_GENERATOR_VERSION}"/topicgenerator-"${TOPIC_GENERATOR_VERSION}".jar
+TOPIC_GENERATOR_CONFIG_ABS_PATH=$(readlink -f ${TOPIC_GENERATOR_CONFIG_PATH})
+
+LONG_DATE=`date +%Y-%m-%dT%H-%M-%S%z`
+TOPIC_GENERATOR_DIR=topicgenerator-"${LONG_DATE}"
+mkdir "${TOPIC_GENERATOR_DIR}"
+cd "${TOPIC_GENERATOR_DIR}"
+
+TOPIC_GENERATOR_JAR=topicgenerator-"${TOPIC_GENERATOR_VERSION}".jar
+wget https://artifacts.oicr.on.ca:443/artifactory/collab-release/io/dockstore/topicgenerator/"${TOPIC_GENERATOR_VERSION}"/"${TOPIC_GENERATOR_JAR}"
 
 echo "Creating a CSV file of AI topic candidates from Dockstore"
-java -jar topicgenerator-*.jar get-topic-candidates
+java -jar "${TOPIC_GENERATOR_JAR}" -c "${TOPIC_GENERATOR_CONFIG_ABS_PATH}" get-topic-candidates
 
 echo "Generating topics"
-java -jar topicgenerator-*.jar generate-topics
+java -jar "${TOPIC_GENERATOR_JAR}" -c "${TOPIC_GENERATOR_CONFIG_ABS_PATH}" generate-topics --entries entries_*.csv
 
 echo "Uploading generated AI topics to Dockstore"
-java -jar topicgenerator-*.jar upload-topics --aiTopics generated-topics_*.csv
+java -jar "${TOPIC_GENERATOR_JAR}" -c "${TOPIC_GENERATOR_CONFIG_ABS_PATH}" upload-topics --aiTopics generated-topics_*.csv
+
+cd ..
+echo "View results in directory ${TOPIC_GENERATOR_DIR}"
