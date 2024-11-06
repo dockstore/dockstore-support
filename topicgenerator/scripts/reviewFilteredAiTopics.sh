76cfcdc29e4e7c7fbffec59abe317a2f6df1a5c9
Kathy Tran <kathylevtran@gmail.com>
2024-11-06 13:01:55 -0500

    Add script

---
+++ topicgenerator/scripts/reviewFilteredAiTopics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SUPPORT_VERSION=${1}
+CONFIG_PATH=${2}
+S3_URI=${3}
+
+wget https://artifacts.oicr.on.ca:443/artifactory/collab-release/io/dockstore/topicgenerator/${SUPPORT_VERSION}/topicgenerator-${SUPPORT_VERSION}.jar
+
+java -jar topicgenerator-${SUPPORT_VERSION}.jar -c ${CONFIG_PATH} upload-topics -ai ${S3_URI} --review
