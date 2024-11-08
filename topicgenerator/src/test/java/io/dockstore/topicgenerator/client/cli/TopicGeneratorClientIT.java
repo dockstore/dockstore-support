488ec42de5ad118c7170b2f5bc5c0c78fe9ed895
Kathy Tran <kathylevtran@gmail.com>
2024-11-08 13:43:52 -0500

    Add command to review filtered topics (#509)
    https://ucsc-cgl.atlassian.net/browse/SEAB-6736
    * Write filtered topics to a different file
    * Ask user for approval
    * Add excluded words
    * Delete generated and reduced poms
    * Ignore generated and dependency reduced poms

--- topicgenerator/src/test/java/io/dockstore/topicgenerator/client/cli/TopicGeneratorClientIT.java
+++ topicgenerator/src/test/java/io/dockstore/topicgenerator/client/cli/TopicGeneratorClientIT.java
@@ -66,20 +66,4 @@ class TopicGeneratorClientIT {
         workflow = workflowsApi.getPublishedWorkflow(32L, null);
         assertNotNull(workflow.getTopicAI());
     }
-
-    @Test
-    void testCensoredAITopics() {
-        final ApiClient apiClient = CommonTestUtilities.getOpenAPIWebClient(true, ADMIN_USERNAME, testingPostgres);
-        final WorkflowsApi workflowsApi = new WorkflowsApi(apiClient);
-
-        Workflow workflow = workflowsApi.getPublishedWorkflow(32L, null);
-        assertNull(workflow.getTopicAI());
-        // This file is modelled after the output file from the "generate-topics" command. It contains 1 row
-        // sentence uses the least offensive offensive word I could find in the Chuck Norris dataset
-        String aiTopicsFilePath = ResourceHelpers.resourceFilePath("offensive-generated-ai-topics.csv");
-
-        TopicGeneratorClient.main(new String[] {"--config", CONFIG_FILE_PATH, "upload-topics", "--aiTopics", aiTopicsFilePath});
-        workflow = workflowsApi.getPublishedWorkflow(32L, null);
-        assertNull(workflow.getTopicAI());
-    }
 }
