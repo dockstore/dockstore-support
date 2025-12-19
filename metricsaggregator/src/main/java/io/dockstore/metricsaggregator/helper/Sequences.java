c94ba81be9abb57f5d1feb70edcde695af4668eb
Stephen Von Worley <svonworl@ucsc.edu>
2025-12-18 19:09:10 -0800

    productize the sequence generation code

--- metricsaggregator/src/main/java/io/dockstore/metricsaggregator/helper/Sequences.java
+++ metricsaggregator/src/main/java/io/dockstore/metricsaggregator/helper/Sequences.java
@@ -3,86 +3,47 @@ package io.dockstore.metricsaggregator.helper;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings({"checkstyle:magicnumber", "checkstyle:whitespacearound"})
-public class LogSequence {
-    private List<Double> getNineStepsTo10() {
-        return List.of(1.3, 1.65, 2.15, 2.8, 3.6, 4.7, 6., 7.8, 10.);
+@SuppressWarnings("checkstyle:magicnumber")
+public final class Sequences {
+
+    private Sequences() {
     }
 
-    private List<Double> getSevenStepsTo6() {
-        return List.of(1.3, 1.65, 2.15, 2.8, 3.6, 4.7, 6.);
+    private static List<Double> get13FriendlyLogStepsTo10() {
+        return List.of(1.2, 1 + 9 / 20., 1.75, 2 + 1 / 12., 2.5, 3., 3 + 7 / 12., 4.25, 5., 6., 7 + 1 / 12., 8 + 5 / 12., 10.);
     }
 
-    /*
-    public List<Double> get13StepsTo10() {
-        return List.of(1.2, 1.45, 1.75, 2.1, 2.5, 3., 3.55, 4.2, 5., 6., 7.15, 8.4, 10.);
-
+    private static List<Double> get10FriendlyLogStepsTo6() {
+        return List.of(1.2, 1 + 9 / 20., 1.75, 2 + 1 / 12., 2.5, 3., 3 + 7 / 12., 4.25, 5., 6.);
     }
 
-    public List<Double> get10StepsTo6() {
-        return List.of(1.2, 1.45, 1.75, 2.1, 2.5, 3., 3.55, 4.2, 5., 6.);
-    }
-    */
-    private List<Double> get13StepsTo10() {
-        return List.of(1.2, 1 + 9/20., 1.75, 2 + 1/12., 2.5, 3., 3 + 7/12., 4.25, 5., 6., 7 + 1/12., 8 + 5/12., 10.);
-
-    }
-
-    private List<Double> get10StepsTo6() {
-        return List.of(1.2, 1 + 9/20., 1.75, 2 + 1/12., 2.5, 3., 3 + 7/12., 4.25, 5., 6.);
-    }
-
-    private List<Double> multiply(double multiplier, List<Double> sequence) {
+    private static List<Double> multiply(double multiplier, List<Double> sequence) {
         return sequence.stream().map(v -> v * multiplier).toList();
     }
 
-    private List<Double> round(List<Double> sequence) {
+    private static List<Double> round(List<Double> sequence) {
         return sequence.stream().map(v -> v.isInfinite() ? v : (double)Math.round(v)).toList();
     }
 
-    public List<Double> getRunTimeLogSequence() {
-        List<Double> values = new ArrayList<>();
-        values.add(0.);
-        for (int i = 0; i < 40; i++) {
-            double x0 = 0.5 * i;
-            double x1 = x0 + 0.25;
-            values.add(Math.pow(2, x0));
-            values.add(Math.pow(2, x1));
-        }
-        return values;
-    }
-
-    /*
-    public List<Double> getFriendlyRunTimeLogSequence() {
-        List<Double> values = new ArrayList<>();
-        values.addAll(List.of(0., 1., 3., 5., 8., 10., 13., 17., 22., 28., 36., 47., 60.)); // <= 60 sec
-        values.addAll(multiply(60, getNineStepsTo10())); // <= 10m
-        values.addAll(multiply(600, getSevenStepsTo6())); // <= 1h
-        values.addAll(multiply(3600, getNineStepsTo10())); // <= 10h
-        values.addAll(multiply(3600, List.of(13., 17., 24.))); // <= 1d
-        values.addAll(multiply(24 * 3600, List.of(2., 4., 10., Double.MAX_VALUE)));
-        return values;
-    }
-    */
-
-    public List<Double> getFriendlyRunTimeLogSequence() {
+    /**
+     * Generate a sequence of increasing numbers wherein successive values increase by approximately the same
+     * multiplicative factor - in other words, they are separated by approximately the same amount in log space.
+     * This sequence will be used to convert the run times of Dockstore workflows, in seconds, into a histogram.
+     * The values at either end of the sequence may include wider "log ranges" and other deviations to accomodate
+     * facts such as "the log of 0 is undefined (or negative infinity, depending on who you talk to)".  The values
+     * that represent 10s to 10h should be very close to their exact exponential counterparts, but may deviate by
+     * small amounts to allow the values to map to as many "simple" time intervals as possible (ex: 50s, 1m30s, etc).
+     * Within the range of 1m to 10h, the multiplier between adjacent values ranges from 1.1764 to 1.2083.
+     */
+    public static List<Double> getFriendlyLogRunTimeSequence() {
         List<Double> values = new ArrayList<>();
         values.addAll(List.of(0., 1., 3., 5., 8., 10., 12., 14., 17., 21., 25., 30., 36., 42., 50., 60.));
-        values.addAll(multiply(60, get13StepsTo10()));
-        values.addAll(multiply(600, get10StepsTo6()));
-        values.addAll(multiply(3600, get13StepsTo10())); // <= 10h
+        values.addAll(multiply(60, get13FriendlyLogStepsTo10()));
+        values.addAll(multiply(600, get10FriendlyLogStepsTo6()));
+        values.addAll(multiply(3600, get13FriendlyLogStepsTo10())); // <= 10h
         values.addAll(multiply(3600, List.of(12., 14.5, 18., 24.))); // <= 1d
         values.addAll(multiply(24 * 3600, List.of(2., 4., 10., 30., 100.)));
         values.add(Double.POSITIVE_INFINITY);
         return round(values);
-    }
-
-    public List<Double> getLinearSequence(double start, double end, double step) {
-        List<Double> values = new ArrayList<>();
-        for (double value = start; value <= end; value += step) {
-            values.add(value);
-        }
-        values.add(Double.POSITIVE_INFINITY);
-        return values;    
     }
 }
--- metricsaggregator/src/test/java/io/dockstore/metricsaggregator/helper/SequencesTest.java
+++ metricsaggregator/src/test/java/io/dockstore/metricsaggregator/helper/SequencesTest.java
@@ -1,16 +1,52 @@
 package io.dockstore.metricsaggregator.helper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
-class LogSequenceTest {
+public class SequencesTest {
+
+    @Test
+    void testFriendlyLogRunTimeSequence() {
+
+        List<Double> sequence = Sequences.getFriendlyLogRunTimeSequence();
+        int size = sequence.size();
+
+        // Range should be from 0 to infinity.
+        assertEquals(0, sequence.get(0));
+        assertEquals(Double.POSITIVE_INFINITY, sequence.get(size - 1));
+
+        // Each value (except the last) should be an integer.
+        for (int i = 0; i < size - 1; i++) {
+            double v = sequence.get(i);
+            assertEquals(Math.round(v), v);
+        }
+
+        // Every successive value should be larger.
+        for (int i = 0; i < size - 1; i++) {
+            double a = sequence.get(i);
+            double b = sequence.get(i + 1);
+            assertTrue(a < b, () -> "%f %f".formatted(a, b));
+        }
+
+        // In the range of 1m (60s) to 10h (36000s), each successive value should increase by a factor of between 1.17 and 1.21.
+        for (int i = 0; i < size - 1; i++) {
+            double a = sequence.get(i);
+            double b = sequence.get(i + 1);
+            if (a >= 60 && a < 36000) {
+                double factor = b / a;
+                assertTrue(factor > 1.17, () -> Double.toString(factor));
+                assertTrue(factor < 1.21, () -> Double.toString(factor));
+            }
+        }
+    }
 
     @Test
     void print() {
-        print(new LogSequence().getRunTimeLogSequence());
-        print(new LogSequence().getFriendlyRunTimeLogSequence());
-        print(new LogSequence().getLinearSequence(0, 500, 5));
+        print(Sequences.getFriendlyLogRunTimeSequence());
     }
 
     void print(List<Double> sequence) {
