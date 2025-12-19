d9e8b6c832ee0fc933b7c9266895360387329641
Stephen Von Worley <svonworl@ucsc.edu>
2025-12-18 19:09:39 -0800

    remove some testing stuff

--- metricsaggregator/src/test/java/io/dockstore/metricsaggregator/helper/SequencesTest.java
+++ metricsaggregator/src/test/java/io/dockstore/metricsaggregator/helper/SequencesTest.java
@@ -43,37 +43,4 @@ public class SequencesTest {
             }
         }
     }
-
-    @Test
-    void print() {
-        print(Sequences.getFriendlyLogRunTimeSequence());
-    }
-
-    void print(List<Double> sequence) {
-        System.out.println("sequence");
-        System.out.println(format(sequence));
-        System.out.println(sequence.size());
-        factors(sequence);
-    }
-
-    String format(List<Double> sequence) {
-        return sequence.stream().map(this::format).collect(Collectors.joining(" "));
-    }
-
-    String format(double value) {
-        int hours = ((int)value) / 3600;
-        value -= hours * 3600;
-        int minutes = ((int)value) / 60;
-        value -= minutes * 60;
-        double seconds = value;
-        return "%2d:%02d:%02f".formatted(hours, minutes, seconds);
-    }
-
-    void factors(List<Double> sequence) {
-        for (int i = 0; i < sequence.size() - 1; i++) {
-            double a = sequence.get(i);
-            double b = sequence.get(i + 1);
-            System.out.println(a + " " + b + " " + b / a);
-        }
-    }
 }
