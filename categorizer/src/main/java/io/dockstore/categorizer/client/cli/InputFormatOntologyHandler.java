9e6c2c00f673c37e8ae87e23bc3410dfdc23416b
Stephen Von Worley <svonworl@ucsc.edu>
2026-05-06 17:06:21 -0700

    simplify input format prompts

--- categorizer/src/main/java/io/dockstore/categorizer/client/cli/InputFormatOntologyHandler.java
+++ categorizer/src/main/java/io/dockstore/categorizer/client/cli/InputFormatOntologyHandler.java
@@ -17,9 +17,9 @@ public class InputFormatOntologyHandler extends ThreeStageOntologyHandler {
             formatEntryData(entryData),
             "",
             "List the input file formats.",
-            "Files within archived or compressed input files are also inputs.",
-            "Omit output formats and exclusively internal formats.",
-            "Be terse and use scientific terminology."
+            "Detail the format variants and format versions.",
+            "Omit output formats.",
+            "Be terse."
         );
     }
 
@@ -30,11 +30,10 @@ public class InputFormatOntologyHandler extends ThreeStageOntologyHandler {
             "Classify the %s's input formats into the following list of categories:".formatted(entryType),
             createTaggedOntologyCsv(nodes, "input-format-"),
             "",
-            "The %s supports the following inputs:".formatted(entryType),
+            "The %s accepts the following inputs:".formatted(entryType),
             tag("input-description", summary),
             "",
             "List the input formats.",
-            "Files within archived or compressed input files are also inputs.",
             "Output one input format ID per line and no other text."
         );
     }
@@ -43,11 +42,10 @@ public class InputFormatOntologyHandler extends ThreeStageOntologyHandler {
     protected String createValidateInstruction(Ontology.Node node, String summary, EntryData entryData) {
         String entryType = entryData.entryType();
         return joinLines(
-            "Use the following description to determine the input formats supported by the %s:".formatted(entryType),
+            "Use the following description to determine the input formats accepted by the %s:".formatted(entryType),
             tag("input-description", summary),
             "",
-            "Does the %s accept an input in the following file format?".formatted(entryType),
-            "Files within archived or compressed input files are also inputs.",
+            "Does the %s accept the following input format?".formatted(entryType),
             "Answer \"yes\" or \"no\" with no other text.",
             tag("format-name", node.label()),
             tag("format-description", node.definition())
